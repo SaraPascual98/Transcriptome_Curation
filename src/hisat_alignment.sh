7f603c7e62d9b07d81c87495b8a31faaae232af1
SaraPascu98 <sarapascu98@gmail.com>
2023-04-02 16:28:13 +0200

    Add comments to the alignment script and build index genome

--- src/hisat_alignment.sh
+++ src/hisat_alignment.sh
@@ -16,16 +16,25 @@ module load SAMtools/1.9-GCC-8.2.0-2.31.1 HISAT2/2.1.0-foss-2019a
 source /home/dcarrasco/miniconda3/bin/activate
 conda activate hisat2
 
+# Creation of the genome index of each haplotype
+
+srun hisat2-build /home/arodriguez/Data_110R/Vrp/VITVix110R_v1.0.pseudomolecules.hap_Vrp.fasta vrp -o /home/arodriguez/Data_110R/Vrp/VITVix110R_v1.0.pseudomolecules.hap_Vrp
+srun hisat2-build /home/arodriguez/Data_110R/Vbe/VITVix110R_v1.0.pseudomolecules.hap_Vrp.fasta vbe -o /home/arodriguez/Data_110R/Vbe/VITVix110R_v1.0.pseudomolecules.hap_Vbe
+
+# Definition of variables
 
 PARAMS=$(sed -n ${SLURM_ARRAY_TASK_ID}p /home/dcarrasco/Resultados/samples_list.txt) # List with the names of all samples to align
-SAMPLES_RES_VRP=/home/dcarrasco/Resultados/Resultados_alineamiento/vrp 
-SAMPLES_RES_VBE=/home/dcarrasco/Resultados/Resultados_alineamiento/vbe
+SAMPLES_RES_VRP=/home/dcarrasco/Resultados/Resultados_alineamiento/vrp  # Output directory for V.rupestris alignment
+SAMPLES_RES_VBE=/home/dcarrasco/Resultados/Resultados_alineamiento/vbe # Output directory for V.berlandieri alignment
 
-INDEX_DIR_VRP=/home/arodriguez/Data_110R/Vrp/VITVix110R_v1.0.pseudomolecules.hap_Vrp
-INDEX_DIR_VBE=/home/arodriguez/Data_110R/Vbe/VITVix110R_v1.0.pseudomolecules.hap_Vbe
+INDEX_DIR_VRP=/home/arodriguez/Data_110R/Vrp/VITVix110R_v1.0.pseudomolecules.hap_Vrp #Index V.rupestris directory
+INDEX_DIR_VBE=/home/arodriguez/Data_110R/Vbe/VITVix110R_v1.0.pseudomolecules.hap_Vbe #Index V.rupestris directory
 
-TRIMMED_DATA_VRP=/home/arodriguez/data_all_experiments/vrp
-TRIMMED_DATA_VBE=/home/arodriguez/data_all_experiments/vbe
+TRIMMED_DATA_VRP=/home/arodriguez/data_all_experiments/vrp # Directory of trimmed reads of V.rupestris
+TRIMMED_DATA_VBE=/home/arodriguez/data_all_experiments/vbe # Directory of trimmed reads of V.berlandieri
+
+
+# Creation of the V.rupestris alignment results folders
 
 if [[ ! -f $SAMPLES_RES_VRP/$PARAMS ]]
 then
@@ -46,6 +55,7 @@ echo $SAMPLES_RES_VRP/$PARAMS/alignment.sam
 
 cd $SAMPLES_RES_VRP/$PARAMS
 
+# Alignment of V.rupestris samples against the reference genome
 
 srun hisat2 --dta --mp 2,1 -x $INDEX_DIR_VRP \
 -1 $TRIMMED_DATA_VRP/$PARAMS/output_forward_paired.fq \
@@ -66,7 +76,7 @@ rm -rf  $SAMPLES_RES_VRP/$PARAMS/alignment.sam
 srun samtools sort $SAMPLES_RES_VRP/$PARAMS/alignment.bam -o $SAMPLES_RES_VRP/$PARAMS/alignment_sorted.bam
 
 
-
+# Creation of the V.berlandieri alignment results folders
 
 if [[ ! -f $SAMPLES_RES_VBE/$PARAMS ]]
 then
@@ -86,6 +96,7 @@ echo $SAMPLES_RES_VBE/$PARAMS/alignment.sam
 
 
 
+# Alignment of V.berlandieri samples against the reference genome
 
 srun hisat2 --dta --mp 2,1 -x $INDEX_DIR_VBE \
 -1 $TRIMMED_DATA_VBE/$PARAMS/output_forward_paired.fq \
